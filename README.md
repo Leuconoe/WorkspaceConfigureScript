726db77a6e7092cc1bfb4d61c17fcfcfab640481
trkim <6531761+Leuconoe@users.noreply.github.com>
2024-09-13 10:22:42 +0900

    fix some

--- README.md
+++ README.md
@@ -2,20 +2,5 @@
 Script to install prerequisites for a new workspace
 
 ```
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Leuconoe/WorkspaceConfigureScript/main/WorkspaceConfigureScript.ps1" -UseBasicP)
-```
-
-
-```
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-$url = "https://raw.githubusercontent.com/Leuconoe/WorkspaceConfigureScript/main/WorkspaceConfigureScript.ps1"
-$localPath = "$env:TEMP\script.ps1"
-
-Invoke-WebRequest -Uri $url -OutFile $localPath
-```
-
-```
-$url = "https://raw.githubusercontent.com/Leuconoe/WorkspaceConfigureScript/main/WorkspaceConfigureScript.ps1"
-curl -o "$env:TEMP\script.ps1" $url
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Leuconoe/WorkspaceConfigureScript/main/WorkspaceConfigureScript.ps1'))
 ```
--- WorkspaceConfigureScript.ps1
+++ WorkspaceConfigureScript.ps1
@@ -1,53 +1,58 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Leuconoe/WorkspaceConfigureScript/main/WorkspaceConfigureScript.ps1'))
+
+
+
 ###############################################################
 ####################### font & app list #######################
 ###############################################################
 # some nerdfonts from https://www.nerdfonts.com/font-downloads
 if (-not $FontPath) {
-$FontPath = @(
-"https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.zip",
-"https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip",
-"https://github.com/Leuconoe/d2codingfont-nerd-fonts/releases/download/D2Coding-Ver1.3.2-20180524-Nerd-Fonts-v3.0.2-168/D2Coding-Ver1.3.2-20180524-Nerd-Fonts-v3.0.2-168.zip"
-)
+	$FontPath = @(
+		"https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.zip",
+		#"https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip",
+		"https://github.com/Leuconoe/d2codingfont-nerd-fonts/releases/download/D2Coding-Ver1.3.2-20180524-Nerd-Fonts-v3.0.2-168/D2Coding-Ver1.3.2-20180524-Nerd-Fonts-v3.0.2-168.zip"
+	)
 }
 if (-not $WingetPackages) {
-$WingetPackages = @(
-"chocolatey",
-"Bandisoft.Bandizip",
-"Bandisoft.Honeyview",
-"Anaconda.Anaconda3", 
-"CoreyButler.NVMforWindows",
-"Notepad\u002B\u002B.Notepad\u002B\u002B",
-"Google.GoogleDrive",
-"Google.Chrome",
-"Google.ChromeRemoteDesktopHost",
-"Unity.UnityHub",
-"Git.Git",
-"TortoiseGit.TortoiseGit",
-"TortoiseSVN.TortoiseSVN",
-"Microsoft.VisualStudioCode",
-"Microsoft.VisualStudio.2022.Community",
-"Microsoft.PowerShell",
-"Microsoft.WindowsTerminal",
-"tailscale.tailscale",
-"junegunn.fzf",
-"Starship.Starship", 
-"JanDeDobbeleer.OhMyPosh"
-)
+	$WingetPackages = @(
+		"nerd-fonts-FiraMono",
+		"nerd-fonts-CascadiaCode",
+		"Bandisoft.Bandizip",
+		"Bandisoft.Honeyview",
+		"Anaconda.Anaconda3", 
+		"CoreyButler.NVMforWindows",
+		"Notepad\u002B\u002B.Notepad\u002B\u002B",
+		"Google.GoogleDrive",
+		"Google.Chrome",
+		"Google.ChromeRemoteDesktopHost",
+		"Unity.UnityHub",
+		"Git.Git",
+		"TortoiseGit.TortoiseGit",
+		"TortoiseSVN.TortoiseSVN",
+		"Microsoft.VisualStudioCode",
+		"Microsoft.VisualStudio.2022.Community",
+		"Microsoft.PowerShell",
+		"Microsoft.WindowsTerminal",
+		"tailscale.tailscale",
+		"junegunn.fzf",
+		"DeepL.DeepL",
+		"Starship.Starship", 
+		"JanDeDobbeleer.OhMyPosh"
+	)
 }
-
 if (-not $ChocoPackages) {
-$ChocoPackages = @(
-    "androidstudio",
-    "qdir"
-)
+	$ChocoPackages = @(
+		"androidstudio",
+		"qdir"
+	)
 }
-
 if (-not $PSModules) {
-$PSModules = @(
-    "Terminal-Icons",  #Terminal-Icons is a PowerShell module that adds file and folder icons when displaying items in the terminal. ( https://github.com/devblackops/Terminal-Icons )
-    "PSReadLine",      # PSReadLine is replaces the command line editing experience of PowerShell for versions 3 and up( https://github.com/PowerShell/PSReadLine )
-    "PSFzf"            # PSFzf is a PowerShell module that wraps fzf, a fuzzy file finder for the command line. ( https://github.com/kelleyma49/PSFzf )
-)
+	$PSModules = @(
+		"PowerShellGet",   # PowerShell module with commands for discovering, installing, updating and publishing the PowerShell artifacts like Modules, DSC Resources, Role Capabilities and Scripts.
+		"Terminal-Icons",  # Terminal-Icons is a PowerShell module that adds file and folder icons when displaying items in the terminal. ( https://github.com/devblackops/Terminal-Icons )
+		"PSReadLine",      # PSReadLine is replaces the command line editing experience of PowerShell for versions 3 and up( https://github.com/PowerShell/PSReadLine )
+		"PSFzf"            # PSFzf is a PowerShell module that wraps fzf, a fuzzy file finder for the command line. ( https://github.com/kelleyma49/PSFzf )
+	)
 }
 
 #########################################################
@@ -102,33 +107,37 @@ function Install-FontsFromURL {
         }
     }
 }
-function Add-MultiLineTextIfMissing {
+function AddPowershellProfile {
     param (
         [string]$FilePath,
-        [string[]]$TextsValues
+        [array]$TextsValues
     )
 
-    # Read the content of the file
-    $content = Get-Content -Path $FilePath -Raw
-
-    foreach ($text in $TextsValues) {
-        # Convert multi-line text into a single string with line breaks preserved
-        $formattedText = $text -join "`n"
-        
-        # Check if the multi-line text is present in the content
-        if ($content -notmatch [regex]::Escape($formattedText)) {
-            # Append the multi-line text to the content
-            $content += "`n$formattedText"
-            Write-Output "Added multi-line text."
-        } else {
-            Write-Output "Multi-line text already present."
-        }
+    # Ensure the profile file exists
+    if (-not (Test-Path $FilePath)) {
+        New-Item -Path $FilePath -ItemType "file" -Force
     }
 
-    # Save the updated content back to the file
-    Set-Content -Path $FilePath -Value $content
-}
+    # Read the content of the profile file. Use -ErrorAction to handle empty files gracefully.
+    $content = ""
+    if ((Get-Item $FilePath).Length -gt 0) {
+        $content = Get-Content -Path $FilePath -Raw
+    }
 
+    foreach ($textValue in $TextsValues) {
+        $comment = $textValue[0]
+        $code = $textValue[1]
+
+        # Check if the profile file contains the comment or code
+        if ($content -notmatch [regex]::Escape($comment)) {
+            # Append the comment and code block to the profile
+            "`n$comment`n$code" | Add-Content -Path $FilePath
+            Write-Output "Added: $comment and $code"
+        } else {
+            Write-Output "Already present: $comment"
+        }
+    }
+}
 #######################################################
 ####################### install start #################
 #######################################################
@@ -138,44 +147,58 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     $newProcess = Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-# Your script code continues here
+
 Write-Host "Running with administrator privileges."
 
-Write-Host "Installing winget fonts..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+
+Write-Host "Installing fonts..."
 foreach ($font in $FontPath) {
 	Install-FontsFromURL -downloadUrl $font
 }
-Write-Host "Installing winget packages..."
-## Throwing a short exception because winget sometimes doesn't update its hash
 
+Write-Host "Installing chocolatey..."
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Write-Host "update winget from chocolatey..."
+choco install winget -y
+
+Write-Host "Installing winget packages..."
 winget settings --enable InstallerHashOverride
 foreach ($package in $WingetPackages) {
-    winget install --id=$package --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --ignore-security-hash --force
+	Write-Host "Installing $package..."
+    winget install --id=$package --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
 }
 winget settings --disable InstallerHashOverride
 
 Write-Host "Installing choco packages..."
 foreach ($package in $ChocoPackages) {
+	Write-Host "Installing $package..."
     choco install $package -y
 }
 
 Write-Host "Installing module..."
 foreach ($module in $PSModules) {
+	Write-Host "Installing $module..."
     Install-Module -Name $module -Force -AllowClobber -Repository PSGallery
 }
 
 ################################################################################
 ####################### open $profile or modify $profile #######################
 ################################################################################
-$profileValues = @(
+$PowershellProfileValues = @(
     @("#Terminal-Icons", "Import-Module -Name Terminal-Icons"),
     @("#fzf", "Import-Module -Name PSFzf"),
-    #@("#oh-my-posh", "oh-my-posh init pwsh --config ~/quick-term.omp.json | Invoke-Expression"),
+	@("#PSReadLine", "Set-PSReadLineOption -PredictionSource History`nSet-PSReadLineOption -PredictionViewStyle ListView"),
+    @("#oh-my-posh", "#oh-my-posh init pwsh --config ~/quick-term.omp.json | Invoke-Expression"),
     @("#starship", "Invoke-Expression (&starship init powershell)")
 )
 
-Add-MultiLineTextIfMissing -FilePath $PROFILE -TextsValues $profileValues
+AddPowershellProfile -FilePath $PROFILE -TextsValues $PowershellProfileValues
 
 #if using starship, add config
-mkdir -p ~/.config | New-Item ~/.config/starship.toml | cd ~/.config/
+mkdir -p ~/.config;New-Item ~/.config/starship.toml;cd ~/.config/
 starship preset gruvbox-rainbow -o starship.toml
+
+
+Read-Host -Prompt "Press Enter to continue"
